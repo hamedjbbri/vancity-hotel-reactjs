c289f48a5ad94ef212b6e2a700b688106f49ad99
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-03-16 09:43:10 -0700

    Components folder structure && navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'; 
 import './App.css';
 
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import Error from './pages/Error'
+import SingleRoom from './pages/SingleRoom'
+
+import {Route, Switch} from 'react-router-dom';
+
+import Navbar from './components/Navbar'
+
 function App() {
   return (
-     <>
-     Hello from app
-     </>
+    <>
+      <Navbar />
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Route component={Error}/>
+      </Switch> 
+       
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import { FaAlignRight } from 'react-icons/fa'
+import {Link} from 'react-router-dom';
+
+
+
+
+
+
+export default class Navbar extends Component {
+    state= {
+        isOpen: false
+    } 
+
+    handleToggle = () => {
+        this.setState({ isOpen: !this.state.isOpen })
+    }
+
+
+    render() {
+        return (
+            <nav className="navbar">
+                <div className="nav-center">
+                    <div className="nav-header"></div>
+                    <Link ro="/"/>
+                    <img src={logo} alt="Beach Resort" />
+
+                </div>
+            </nav>
+        )
+    }
+}
