5b906e900ceab5f1cbdc35f39253b11774d5923a
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-03-19 21:27:47 -0700

    Featured images section

--- src/components/FeaturedRoom.js
+++ src/components/FeaturedRoom.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import {RoomContext} from '../context';
-
+import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRoom extends Component {
     
     static contextType = RoomContext;
     
     render() {
-        const {name,greeting} = this.context;
-       
-    
-        return <div>{greeting} {name} from FeaturedRoom</div>
-        
+        let { loading, featuredRooms: rooms} = this.context; 
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })  
+      
+        return (
+            <section className="featured-rooms">  
+                 <Title title="featured rooms" />
+                 <div className="featured-rooms-center">
+                     { loading ? <Loading /> : rooms }
+                 </div>
+ 
+            </section>
+        );        
     }
 }
 
